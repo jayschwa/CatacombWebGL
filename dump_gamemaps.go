4a986b39688d2ad1cdc71b0be951c69b914e4ae2
Jay Weisskopf <jay@jayschwa.net>
2016-12-13 02:52:39 -0500

    RLEWExpand and print map data

--- dump_gamemaps.go
+++ dump_gamemaps.go
@@ -48,8 +48,8 @@ func MapHeaders(c3d []byte) (headers []*MapHeader, err error) {
 	return headers, nil
 }
 
-const NearPointer = 0xa7
-const FarPointer = 0xa8
+const NearPointer = 0xA7
+const FarPointer = 0xA8
 
 func CarmackExpand(dst, src []byte) error {
 	di, si := 0, 0
@@ -84,6 +84,40 @@ func CarmackExpand(dst, src []byte) error {
 	return nil
 }
 
+func RLEWExpand(dst, src []byte) error {
+	words := make([]uint16, len(src)/2)
+	err := binary.Read(bytes.NewBuffer(src), binary.LittleEndian, words)
+	size := int(words[0])
+	if len(dst) != size {
+		return fmt.Errorf("dst buffer length %v does not match prefixed length %v in compressed data", len(dst), size)
+	}
+	words = words[1:]
+	if err != nil {
+		panic(err)
+	}
+	di, si := 0, 0
+	for si < len(words) {
+		word := words[si]
+		si += 1
+		if word == 0xABCD {
+			count := int(words[si])
+			data := words[si+1]
+			si += 2
+			for i := 0; i < count; i++ {
+				binary.LittleEndian.PutUint16(dst[di:], data)
+				di += 2
+			}
+		} else {
+			binary.LittleEndian.PutUint16(dst[di:], word)
+			di += 2
+		}
+	}
+	if di != len(dst) {
+		return fmt.Errorf("dst buffer has length of %v but was filled to %v", len(dst), di)
+	}
+	return nil
+}
+
 func main() {
 	c3d, err := ioutil.ReadFile("GAMEMAPS.C3D")
 	if err != nil {
@@ -95,6 +129,7 @@ func main() {
 	}
 	for _, m := range maps {
 		fmt.Println(m.Name())
+		var planes [3][]byte
 		for i := 0; i < 3; i += 2 {
 			fmt.Println("Plane", i)
 			start := int(m.PlaneStart[i])
@@ -109,6 +144,38 @@ func main() {
 			if err != nil {
 				fmt.Println(err)
 			}
+			planeWords := make([]byte, m.Len())
+			err = RLEWExpand(planeWords, expandedData)
+			if err != nil {
+				fmt.Println(err)
+			}
+			planes[i] = make([]byte, len(planeWords)/2)
+			for k := range planes[i] {
+				if planeWords[k*2+1] != 0 {
+					panic(fmt.Errorf("word %v on plane %v of %v is not zero", k, i, m.Name()))
+				}
+				planes[i][k] = planeWords[k*2] // condense down to bytes - high byte is never used
+			}
+		}
+
+		// Draw text map
+		for h := 0; h < int(m.Height); h++ {
+			for w := 0; w < int(m.Width); w++ {
+				idx := w + h*int(m.Width)
+				d := planes[0][idx]
+				if d > 0x20 {
+					d = 0
+				}
+				if d == 0 {
+					d = planes[2][idx]
+				}
+				if d == 0 {
+					fmt.Print("  ")
+				} else {
+					fmt.Printf("%02X", d)
+				}
+			}
+			fmt.Println()
 		}
 		fmt.Println()
 	}
