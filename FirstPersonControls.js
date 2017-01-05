e2cba18aa195ecd12521049f1c4579f35325df69
Jay Weisskopf <jay@jayschwa.net>
2017-01-04 23:17:53 -0500

    Support dvorak layout for controls

--- FirstPersonControls.js
+++ FirstPersonControls.js
@@ -143,16 +143,19 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 		switch ( event.keyCode ) {
 
 			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = true; break;
+			case 87: /*W*/
+			case 188: /*comma*/ this.moveForward = true; break;
 
 			case 37: /*left*/
 			case 65: /*A*/ this.moveLeft = true; break;
 
 			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = true; break;
+			case 83: /*S*/
+			case 79: /*O*/ this.moveBackward = true; break;
 
 			case 39: /*right*/
-			case 68: /*D*/ this.moveRight = true; break;
+			case 68: /*D*/
+			case 69: /*E*/ this.moveRight = true; break;
 
 			case 82: /*R*/ this.moveUp = true; break;
 			case 70: /*F*/ this.moveDown = true; break;
@@ -166,16 +169,19 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 		switch ( event.keyCode ) {
 
 			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = false; break;
+			case 87: /*W*/
+			case 188: /*comma*/ this.moveForward = false; break;
 
 			case 37: /*left*/
 			case 65: /*A*/ this.moveLeft = false; break;
 
 			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = false; break;
+			case 83: /*S*/
+			case 79: /*O*/ this.moveBackward = false; break;
 
 			case 39: /*right*/
-			case 68: /*D*/ this.moveRight = false; break;
+			case 68: /*D*/
+			case 69: /*E*/ this.moveRight = false; break;
 
 			case 82: /*R*/ this.moveUp = false; break;
 			case 70: /*F*/ this.moveDown = false; break;
