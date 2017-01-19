65ac5933a28f96e8e4144ef6bbca19b0511e2367
Jay Weisskopf <jay@jayschwa.net>
2017-01-19 02:00:44 -0500

    Minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import nodeResolve from "rollup-plugin-node-resolve";
+import uglify from "rollup-plugin-uglify";
+import { minify } from "uglify-js";
 
 export default {
   entry: "src/cat3d.js",
   dest: "build/cat3d.js",
   format: "iife",
   moduleName: "Catacomb3D",
-  sourceMap: "inline",
-  plugins: [nodeResolve()]
+  sourceMap: true,
+  plugins: [
+  	nodeResolve(),
+  	uglify({}, minify)
+  ]
 };
