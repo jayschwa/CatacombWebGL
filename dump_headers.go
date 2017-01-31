45e3fac5a4a10c96040d76016448dc6e25f0b516
Jay Weisskopf <jay@jayschwa.net>
2017-01-30 19:37:32 -0500

    Dump audio

--- dump_headers.go
+++ dump_headers.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	exe, err := ioutil.ReadFile("CAT3D.EXE")
+	exe, err := ioutil.ReadFile("cat3d.exe")
 	if err != nil {
 		panic(err)
 	}
@@ -24,11 +24,33 @@ func main() {
 		panic(err)
 	}
 
-	egadict_last_bytes := []byte{0xFD, 0x01, 0x00, 0x00, 0x00, 0x00}
-	egadict_end_idx := bytes.LastIndex(exe, egadict_last_bytes) + len(egadict_last_bytes)
+	dict_last_bytes := []byte{0xFD, 0x01, 0x00, 0x00, 0x00, 0x00}
+
+	egadict_end_idx := bytes.LastIndex(exe, dict_last_bytes) + len(dict_last_bytes)
 	egadict_start_idx := egadict_end_idx - 1024
 	fmt.Printf("%#x to %#x\n", egadict_start_idx, egadict_end_idx)
 	err = ioutil.WriteFile("EGADICT.C3D", exe[egadict_start_idx:egadict_end_idx], 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	audiohead_last_bytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(audiohead_last_bytes, 5062)
+	//audiohead_last_bytes = audiohead_last_bytes[:3]
+
+	audiohead_end_idx := bytes.LastIndex(exe, audiohead_last_bytes) + len(audiohead_last_bytes)
+	audiohead_start_idx := bytes.LastIndex(exe[:audiohead_end_idx], []byte{0x00, 0x00, 0x00, 0x00})
+	fmt.Printf("%#x to %#x (%v bytes)\n", audiohead_start_idx, audiohead_end_idx, audiohead_end_idx-audiohead_start_idx)
+	err = ioutil.WriteFile("AUDIOHEAD.C3D", exe[audiohead_start_idx:audiohead_end_idx], 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	space := exe[:egadict_start_idx]
+	audiodict_end_idx := bytes.LastIndex(space, dict_last_bytes) + len(dict_last_bytes)
+	audiodict_start_idx := audiodict_end_idx - 1024
+	fmt.Printf("%#x to %#x\n", audiodict_start_idx, audiodict_end_idx)
+	err = ioutil.WriteFile("AUDIODICT.C3D", space[audiodict_start_idx:audiodict_end_idx], 0666)
 	if err != nil {
 		panic(err)
 	}
